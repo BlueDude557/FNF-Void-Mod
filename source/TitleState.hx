e0379488fe1f8b4af2738da66af7ebb9a32a68dd
BlueDude557 <bluehammer557@gmail.com>
2021-08-29 22:50:07 -0400

    yass

--- source/TitleState.hx
+++ source/TitleState.hx
@@ -146,7 +146,7 @@ class TitleState extends MusicBeatState
 		// logoBl.color = FlxColor.BLACK;
 
 		gfDance = new FlxSprite(FlxG.width * 0.4, FlxG.height * 0.07);
-		gfDance.frames = Paths.getSparrowAtlas('gfDanceTitle');
+		gfDance.frames = Paths.getSparrowAtlas('gfDanceTitle' + FlxG.random.int(1, 4));
 		gfDance.animation.addByIndices('danceLeft', 'gfDance', [30, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14], "", 24, false);
 		gfDance.animation.addByIndices('danceRight', 'gfDance', [15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29], "", 24, false);
 		if(FlxG.save.data.antialiasing)
