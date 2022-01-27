50be755bfe4cd85fa0ee497a88cfb5626606e8be
BlueDude557 <bluehammer557@gmail.com>
2022-01-26 21:40:41 -0500

    okay got it lol

--- source/MainMenuState.hx
+++ source/MainMenuState.hx
@@ -77,7 +77,7 @@ class MainMenuState extends MusicBeatState
 		camFollow = new FlxObject(0, 0, 1, 1);
 		add(camFollow);
 
-		magenta = new FlxSprite(-80).loadGraphic(Paths.image('menuDesat'));
+		magenta = new FlxSprite(-80).loadGraphic(Paths.image('menuBGMagenta'));
 		magenta.scrollFactor.x = 0;
 		magenta.scrollFactor.y = 0.10;
 		magenta.setGraphicSize(Std.int(magenta.width * 1.1));
@@ -88,7 +88,7 @@ class MainMenuState extends MusicBeatState
 			{
 				magenta.antialiasing = true;
 			}
-		magenta.color = 0xFFfd719b;
+		//magenta.color = 0xFFfd719b;
 		add(magenta);
 		// magenta.scrollFactor.set();
 
