e871ea588c81bc834c68a3e7ccdcbb11092f2c6f
BlueDude557 <bluehammer557@gmail.com>
2022-05-23 02:42:58 -0400

    and this

--- source/EndTrueSubState.hx
+++ source/EndTrueSubState.hx
@@ -14,23 +14,34 @@ import flixel.addons.transition.FlxTransitionableState;
 class EndTrueSubState extends MusicBeatState
 {
 	public static var leftState:Bool = false;
+	
+	var thankyou:FlxSprite;
+	var txt:FlxText;
+	var white:FlxSprite;
 
 	override function create()
 	{
 		super.create();
-		var bg:FlxSprite = new FlxSprite().loadGraphic(Paths.image('voidend', 'shared'));
+		var bg:FlxSprite = new FlxSprite().loadGraphic(Paths.image('voidtrueend', 'shared'));
 		bg.antialiasing = true;
 		bg.screenCenter();
 		add(bg);
 
-		var txt:FlxText = new FlxText(0, 380, FlxG.width,
-			"After a long and intense show, Bf had out-rapped the legendary A.C. Void! (Who"
-			+ "\nrightfully seemed pretty livid losing for the first time in centuries.)"
-			+ "\n\nEveryone got an incredible performance, and Bf and Gf casually returned"
-			+ "\nto earth and ordered a pizza. Everyone was happy. (Except Void. Obviously.)"
-			+ "\n\nTHE END(?)"
-			+ "\n\n\nYou've got skills... but Void's not finished just yet."
-			+ "\nThink you can beat the true A.C. Void?",
+		thankyou = new FlxSprite().loadGraphic(Paths.image('thankyou', 'shared'));
+		thankyou.antialiasing = true;
+		thankyou.alpha = 0;
+		thankyou.screenCenter();
+		add(thankyou);
+
+		txt = new FlxText(0, 340, FlxG.width,
+			"After an all-out, frenzied encore, 'A.C.' was incapacitated. At last, his cosmic"
+			+ "\nfury was completely spent. Bf had defeated the TRUE A.C. Void!"
+			+ "\n\nAfter several minutes of Bf and Gf talking about what to order for dinner,"
+			+ "\n'A.C.' was gone, and Void finally came to. Aware of how badly things could've"
+			+ "\ngone, he begrudgingly thanked Bf for not letting him tear the galaxy in half."
+			+ "\nBf thanked Void for a fun twelve-ish minutes, to which Void just sighed."
+			+ "\n\nTHE END (for real.)"
+			+ "\n\n\n...One more thing, be sure to check freeplay! (And the files, maybeee?)",
 			28);		
 		txt.setFormat("VCR OSD Mono", 28, FlxColor.fromRGB(200, 200, 200), CENTER);
 		txt.borderColor = FlxColor.BLACK;
@@ -46,19 +57,38 @@ class EndTrueSubState extends MusicBeatState
 			else  FlxTween.tween(txt, {y: 395}, 3.3, 
 				{ease: FlxEase.quadInOut});
 		}, 0);
+
+		white = new FlxSprite(-500, -300).makeGraphic(FlxG.width * 3, FlxG.height * 3, FlxColor.WHITE);
+		white.scrollFactor.set();
+		white.alpha = 0;
+		add(white);
 	}
 
 	override function update(elapsed:Float)
 	{
 		if (controls.ACCEPT)
 		{
-			transIn = FlxTransitionableState.defaultTransIn;
-			transOut = FlxTransitionableState.defaultTransOut;
+			if (thankyou.alpha == 0)
+			{
+				white.alpha = 1;
 
-			leftState = true;
+				thankyou.alpha = 1;
 
-			Conductor.changeBPM(120);
-			FlxG.switchState(new MainMenuState());
+				txt.alpha = 0;
+
+				FlxTween.tween(white, {alpha: 0}, 1.0, 
+					{ease: FlxEase.quadIn});
+			}
+			else
+			{
+				transIn = FlxTransitionableState.defaultTransIn;
+				transOut = FlxTransitionableState.defaultTransOut;
+	
+				leftState = true;
+	
+				Conductor.changeBPM(120);
+				FlxG.switchState(new MainMenuState());
+			}
 		}
 		super.update(elapsed);
 	}
